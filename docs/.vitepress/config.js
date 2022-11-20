e78d21590d67bdba8f329d7b131e72283ef49faf
Jonathan Gober <gober.jonathan@gmail.com>
2022-11-20 16:05:46 -0500

    addomg updates

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,7 @@
 export default {
     title: 'Documentation-Biome',
-    description: 'Just playing around.'
+    description: 'Just playing around.',
+    markdown: {
+      lineNumbers: true
+    }
   }
