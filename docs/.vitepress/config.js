0dd5ac749c4855626d8ee627c7a3edec214f65b3
Jonathan Gober <gober.jonathan@gmail.com>
2022-11-19 22:58:13 -0500

    learning vipress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,20 @@
+export default {
+  title: 'Vi Tutorial',
+  description: 'Just playing around.',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about' },
+      { text: 'Contact', link: '/contact' },
+    ],
+    sidebar: [
+      {
+        text: 'Guides',
+        items: [
+          { text: 'Our Story', link: '/our-story' },
+          { text: 'About', link: '/about' },
+        ]
+      }
+    ]
+  }
+}
