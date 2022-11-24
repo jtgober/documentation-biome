6e1ddc3631e00d1f306cfd9e46e71598cc2289d5
jtgober <jtgober@users.noreply.github.com>
2022-11-24 12:56:32 +0000

    deploy: ca767ca2012463c0fe1024002d3b67f48379e246

---
+++ assets/day2_scope.md.2fd08a57.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as n,e as l}from"./app.db8cecba.js";const F=JSON.parse('{"title":"Scope","description":"","frontmatter":{},"headers":[],"relativePath":"day2/scope.md"}'),e={name:"day2/scope.md"},p=l("",19),o=[p];function t(r,c,i,y,D,C){return a(),n("div",null,o)}const d=s(e,[["render",t]]);export{F as __pageData,d as default};
