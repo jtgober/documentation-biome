8d1cc2bcee93f8d258411fb1fd4030b61ae234b0
jtgober <jtgober@users.noreply.github.com>
2022-11-24 13:23:00 +0000

    deploy: fe613891289d7421244a57bb2f9ab4382bf45879

---
+++ assets/day3_area-calc.md.c0175b2c.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as n,e as l}from"./app.db8cecba.js";const D=JSON.parse('{"title":"Area Calc Answer","description":"","frontmatter":{},"headers":[],"relativePath":"day3/area-calc.md"}'),p={name:"day3/area-calc.md"},o=l("",3),e=[o];function c(r,t,y,C,F,A){return a(),n("div",null,e)}const d=s(p,[["render",c]]);export{D as __pageData,d as default};
